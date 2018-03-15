95e54fb00965c3ed6c5a8674d4f34b49585f4497
jielmn@aliyun.com <jielmn@aliyun.com>
2018-03-15 11:47:37 +0800

    盘点 - 取消对读卡器的串口的配置

--- TagsInventory/TagsInventory/MyReader.cpp
+++ TagsInventory/TagsInventory/MyReader.cpp
@@ -3,6 +3,35 @@
 
 #define  RECONNECT_READER_TIME               10000
 
+static BOOL GetAllSerialPortName(std::vector<std::string> & vCom) {
+
+	// TODO: 在此添加控件通知处理程序代码
+	HKEY hKey = NULL;
+	vCom.clear();
+
+	if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, TEXT("HARDWARE\\DEVICEMAP\\SERIALCOMM"), 0, KEY_READ, &hKey) != ERROR_SUCCESS)
+	{
+		return FALSE;
+	}
+
+	CString valuename, databuffer;
+	DWORD valuenamebufferlength = 200, valuetype, databuddersize = 200;
+
+	int i = 0;
+	while (RegEnumValue(hKey, i++, valuename.GetBuffer(200), &valuenamebufferlength, NULL, &valuetype, (BYTE*)databuffer.GetBuffer(200), &databuddersize) != ERROR_NO_MORE_ITEMS)
+	{
+		std::string com_name = databuffer;
+		vCom.push_back(com_name);
+
+		databuddersize = 200;
+		valuenamebufferlength = 200;
+	}
+
+	RegCloseKey(hKey);
+	return TRUE;
+}
+
+
 void MY_TAG_REPORT(void *cbParam, RFID_READER_HANDLE hreader, DWORD AIPtype, DWORD TagType, DWORD AntId, BYTE uid[], WORD uidlen) {
 	TagItem * pItem = new TagItem;
 	if (0 == pItem) {
@@ -43,9 +72,41 @@ CMyReader::~CMyReader() {
 }
 
 void CMyReader::Open() {
-	char buf[8192];
+	//char buf[8192];
 	err_t iret = NO_ERR;
 
+	std::vector<std::string>  vCom;
+	GetAllSerialPortName(vCom);
+
+	const std::string PARAMS = "RDType=M201;CommType=COM;BaudRate=38400;Frame=8E1;BusAddr=255;COMName=";
+
+	m_nStatus = STATUS_CLOSE;
+	std::vector<std::string>::iterator it;
+	for ( it = vCom.begin(); it != vCom.end(); it++ ) {
+		std::string  sItem = *it;
+		std::string  sFullParam = PARAMS+sItem;
+
+		iret = RDR_Open(sFullParam.c_str(), &m_hr_reader);
+		//iret = RDR_Open("RDType=M201;CommType=COM;COMName=COM3;BaudRate=38400;Frame=8E1;BusAddr=255", &m_hr_reader);
+		// 如果OK
+		if (NO_ERR == iret) {
+			m_nStatus = STATUS_OPEN;
+			// 盘点
+			g_thrd_reader->PostMessage(g_handler_reader, MSG_READER_INVENTORY);
+			break;
+		}
+	}
+
+	if (m_nStatus == STATUS_CLOSE) {
+		// 重连
+		g_thrd_reader->PostDelayMessage(RECONNECT_READER_TIME, g_handler_reader, MSG_RECONNECT_READER);
+	}
+
+	// 通知界面
+	::PostMessage(g_hDlg, UM_SHOW_STATUS, STATUS_TYPE_READER, m_nStatus);
+
+
+	/*
 	g_cfg->GetConfig("reader string", buf, sizeof(buf));
 	iret = RDR_Open(buf, &m_hr_reader);
 	// 如果OK
@@ -62,7 +123,7 @@ void CMyReader::Open() {
 	}
 	// 通知界面
 	::PostMessage(g_hDlg, UM_SHOW_STATUS, STATUS_TYPE_READER, m_nStatus);
-	
+	*/
 }
 
 void CMyReader::Inventory() {
