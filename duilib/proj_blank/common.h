d14f5f7598a8b1ab15478e514d286cd2a696b132
jielmn@aliyun.com <jielmn@aliyun.com>
2018-06-06 16:37:11 +0800

    duilib - 修改

--- duilib/proj_blank/common.h
+++ duilib/proj_blank/common.h
@@ -1,5 +1,8 @@
 #pragma once
 
+#include <vector>
+#include <algorithm>
+
 #include "LmnCommon.h"
 #include "LmnConfig.h"
 #include "LmnLog.h"
@@ -12,3 +15,17 @@
 extern ILog    * g_log;
 extern IConfig * g_cfg;
 extern LmnToolkits::Thread *  g_thrd_db;
+
+
+extern char * Time2String(char * szDest, DWORD dwDestSize, const time_t * t);
+
+// templates
+template <class T>
+void ClearVector(std::vector<T> & v) {
+	typedef std::vector<T>::iterator v_it;
+	v_it it;
+	for (it = v.begin(); it != v.end(); it++) {
+		delete *it;
+	}
+	v.clear();
+}
