e6b9769abb0c7056c35e4243e74a9425281d2113
jielmn@aliyun.com <jielmn@aliyun.com>
2018-09-04 09:44:01 +0800

    修改

--- duilib/proj_wait_progress/main.cpp
+++ duilib/proj_wait_progress/main.cpp
@@ -77,14 +77,14 @@ private:
 };
 
 
-
+ 
 
 class CDuiFrameWnd : public WindowImplBase
 {
 public:
 	virtual LPCTSTR    GetWindowClassName() const { return _T("DUIMainFrame"); }
 	virtual CDuiString GetSkinFile() { return _T("mainframe_wait_progress.xml"); }
-	virtual CDuiString GetSkinFolder() { return _T("res"); }
+	virtual CDuiString GetSkinFolder() { return _T("res\\proj_wait_progress_res"); }
 
 	virtual void    Notify(TNotifyUI& msg) {
 		if (msg.sType == _T("click"))
