1f38337418abed2303142f4a773e4219e51ac05c
jielmn@aliyun.com <jielmn@aliyun.com>
2019-01-14 18:21:26 +0800

    微信小程序 - 服务器重置后。重新装了mysql5.7和apache.tomcat.9.0

--- WeixinApp/Todolist_Company_Server/make.sh
+++ WeixinApp/Todolist_Company_Server/make.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
-javac -classpath ../../lib/servlet-api.jar:$JAVA_HOME/lib/mysql-connector-java-5.1.45-bin.jar:$JAVA_HOME/lib/json.jar:$JAVA_HOME/lib/httpclient-4.5.5.jar:$JAVA_HOME/lib/httpcore-4.4.9.jar:$JAVA_HOME/lib/commons-fileupload-1.3.3.jar  -sourcepath src -d WEB-INF/classes src/mypack/MainServlet.java
+if [ $# -eq 1 ];then
+   if [ $1 == "clean" ];then
+     cd tmp
+     rm -fr *.png
+     rm -fr *.jpg
+   fi
+elif [ $# -eq 0 ];then
+   javac -classpath ../../lib/servlet-api.jar:mysql-connector-java-8.0.13.jar:WEB-INF/lib/json.jar:WEB-INF/lib/httpclient-4.5.5.jar:WEB-INF/lib/httpcore-4.4.9.jar:WEB-INF/lib/commons-fileupload-1.3.3.jar  -sourcepath src -d WEB-INF/classes src/mypack/MainServlet.java
+fi
