398a9340edbdcb08fb5defd3ef2ec5c6c25ff92b
jielmn@aliyun.com <jielmn@aliyun.com>
2019-03-22 09:28:19 +0800

    软件网上管理 - 修改数据库字段长度

--- 软件网上管理/db-scripts/software-manage.sql
+++ 软件网上管理/db-scripts/software-manage.sql
@@ -5,8 +5,8 @@ use software_manage;
 -- 是否开放注册
 create table items ( 
 	id int primary key not null auto_increment, 
-	title varchar(32) not null,
-	abstract varchar(32) not null,
+	title varchar(100) not null,
+	abstract varchar(200) not null,
 	content varchar(4000) not null,
 	create_time datetime not null,
 	software_path varchar(100) not null,
