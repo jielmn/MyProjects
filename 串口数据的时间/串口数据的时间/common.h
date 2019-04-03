e0bdb50086519226fba68fa66b8af5bc87171191
jielmn@aliyun.com <jielmn@aliyun.com>
2019-04-03 18:04:02 +0800

    串口数据的时间

---
+++ 串口数据的时间/串口数据的时间/common.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+
+#include "LmnCommon.h"
+#include "LmnConfig.h"
+#include "LmnLog.h"
+#include "LmnThread.h"
+#include "LmnString.h"
+
+#include "UIlib.h"
+using namespace DuiLib;
+
+#define   PROJ_NAME               "comTime"
+#define   LOG_FILE_NAME           (PROJ_NAME ".log")
+#define   CONFIG_FILE_NAME        (PROJ_NAME ".cfg")
+#define   MAIN_CLASS_WINDOW_NAME  "DUIMainFrame"
+#define   SKIN_FILE               ("mainframe_" PROJ_NAME ".xml")
+#define   SKIN_FOLDER             ("res\\proj_" PROJ_NAME "_res")
+
+
+class  CGlobalData {
+public:
+	ILog    *                 m_log;
+	IConfig *                 m_cfg;
+	LmnToolkits::Thread *     m_thrd_db;
+
+public:
+	CGlobalData() {
+		m_log = 0;
+		m_cfg = 0;
+		m_thrd_db = 0;
+	}
+};
+
+extern CGlobalData  g_data;
+
+extern BOOL EnumPortsWdm(std::vector<std::string> & v);
+extern void SetComboCom(DuiLib::CComboUI * pCombo, std::vector<std::string> & vComPorts);
+
+// templates
+template <class T>
+void ClearVector(std::vector<T> & v) {
+	typedef std::vector<T>::iterator v_it;
+	v_it it;
+	for (it = v.begin(); it != v.end(); it++) {
+		delete *it;
+	}
+	v.clear();
+}
---
+++ 串口数据的时间/串口数据的时间/main.cpp
@@ -0,0 +1,93 @@
+// win32_1.cpp : 定义应用程序的入口点。
+//
+
+#ifdef _DEBUG
+#include "vld.h"
+#endif
+
+#include "Windows.h"
+#include "main.h"
+#include "business.h"
+#include "resource.h"
+
+CDuiFrameWnd::CDuiFrameWnd() {
+	         
+}
+
+CDuiFrameWnd::~CDuiFrameWnd() {
+
+}
+
+void  CDuiFrameWnd::InitWindow() {
+	m_cmbCom = static_cast<DuiLib::CComboUI*>(m_PaintManager.FindControl("cmbComPort"));
+
+	const BYTE * p = (const BYTE *)"abcdefg1234567890";
+	char buf[256];
+	DebugStream(buf, sizeof(buf), p, 17);
+	m_PaintManager.FindControl("test")->SetText(buf);
+
+	OnDeviceChanged(0,0);   
+	WindowImplBase::InitWindow();
+}
+
+CControlUI * CDuiFrameWnd::CreateControl(LPCTSTR pstrClass) {
+	return WindowImplBase::CreateControl(pstrClass);
+}
+
+void CDuiFrameWnd::Notify(TNotifyUI& msg) {
+	WindowImplBase::Notify(msg);
+}
+
+LRESULT CDuiFrameWnd::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	if (uMsg == WM_DEVICECHANGE) {
+		OnDeviceChanged(wParam, lParam);
+	}
+	return WindowImplBase::HandleMessage(uMsg,wParam,lParam);
+}
+  
+void  CDuiFrameWnd::OnDeviceChanged(WPARAM wParm, LPARAM  lParam) {
+	std::vector<std::string> vComPorts;
+	EnumPortsWdm(vComPorts);
+
+	SetComboCom(m_cmbCom, vComPorts);
+}
+
+
+
+
+
+
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+	_In_opt_ HINSTANCE hPrevInstance,
+	_In_ LPWSTR    lpCmdLine,
+	_In_ int       nCmdShow)
+{
+	LmnToolkits::ThreadManager::GetInstance();
+	CBusiness::GetInstance()->Init();
+	g_data.m_log->Output(ILog::LOG_SEVERITY_INFO, "main begin.\n");
+
+	CPaintManagerUI::SetInstance(hInstance);
+	HICON hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_ICON1));
+
+	CDuiFrameWnd * duiFrame = new CDuiFrameWnd;
+	duiFrame->Create(NULL, _T("DUIWnd"), UI_WNDSTYLE_FRAME, WS_EX_WINDOWEDGE);
+	duiFrame->CenterWindow();
+
+	// 设置icon
+	::SendMessage(duiFrame->GetHWND(), WM_SETICON, ICON_BIG, (LPARAM)hIcon);
+	::SendMessage(duiFrame->GetHWND(), WM_SETICON, ICON_SMALL, (LPARAM)hIcon);
+
+	duiFrame->ShowModal();
+	delete duiFrame;
+
+	g_data.m_log->Output(ILog::LOG_SEVERITY_INFO, "main close.\n");
+
+	CBusiness::GetInstance()->DeInit();
+	delete CBusiness::GetInstance();
+	LmnToolkits::ThreadManager::ReleaseInstance();
+
+	return 0;
+}
+
+
