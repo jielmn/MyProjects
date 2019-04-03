47160b83ccaf0a9b55ae60afe4a19bb3d8c78e14
jielmn@aliyun.com <jielmn@aliyun.com>
2019-04-03 16:55:34 +0800

    修改

--- 术中监控_多窗口_4.2/模拟读卡器/main.cpp
+++ 术中监控_多窗口_4.2/模拟读卡器/main.cpp
@@ -52,27 +52,7 @@ static  int   g_HandDelay[MAX_HAND_TAG_CNT] = {10000, 11000, 12000,13000,14000,1
 static  DWORD  g_Temp[180] = { 0 };
 
 DWORD  GetRandTemp(BYTE  byBed) {
-	int  delta     = GetRand(0, 30);
-	BOOL bPositive = (BOOL)GetRand(0, 1);
-
-	if ( g_Temp[byBed] == 0) {
-		g_Temp[byBed] = GetRand(3200, 4080);
-	}
-	
-	if (bPositive) {
-		g_Temp[byBed] += delta;
-	}
-	else {
-		g_Temp[byBed] -= delta;
-	}
-
-	if ((int)g_Temp[byBed] < 3200) {
-		g_Temp[byBed] = 3200;
-	}
-	else if ((int)g_Temp[byBed] > 4080) {
-		g_Temp[byBed] = 4080;
-	}
-
+	g_Temp[byBed] = GetRand(3000 + byBed * 100, 3100  + byBed * 100);
 	return g_Temp[byBed];
 }
 
@@ -127,10 +107,10 @@ void MyMessageHandler::OnMessage(DWORD dwMessageId, const LmnToolkits::MessageDa
 			else if ( byData[5] == 0x02 ) {
 				DWORD r = GetRand(1, 5);
 				if ( r > 1 ) {
-					memcpy(byData, "\x55\x1A\x00\x06\x01\x45\x52\x00\x00\x03\x00\x00\x00\x00\x00\x01\x8F\x50\xD9\x93\xCD\x59\x02\xE0\x02\x07\x08\x05\xFF\xDD\xAA", 31);
+					memcpy(byData, "\x55\x1A\x00\x06\x01\x45\x52\x00\x00\x03\x00\x00\x00\x00\x00\x01\x8F\x50\xD9\x93\xCD\x59\x02\xE0\x02\x07\x08\x05\xFF", 29);
 					byData[3] = byBed;
 					byData[4] = byArea;
-					dwWriteLen = 31;
+					dwWriteLen = 29;
 					//DWORD dwTemp = GetRand(3200, 4080);
 					DWORD dwTemp = GetRandTemp(byBed);
 					byData[24] = (BYTE)(dwTemp / 1000);
