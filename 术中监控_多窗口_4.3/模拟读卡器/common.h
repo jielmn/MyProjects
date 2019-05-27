3b778fff6b81dacf3e4234315d8edfcdbfcd714d
jielmn@aliyun.com <jielmn@aliyun.com>
2019-05-27 09:47:25 +0800

    术中4.3 - 模拟读卡器/发射器

---
+++ 术中监控_多窗口_4.3/模拟读卡器/common.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include <vector>
+#include <algorithm>
+
+#include "LmnCommon.h"
+#include "LmnConfig.h"
+#include "LmnLog.h"
+#include "LmnThread.h"
+#include "LmnString.h"
+
+#define   PROJ_NAME               "simulation"
+#define   LOG_FILE_NAME           (PROJ_NAME ".log")
+#define   CONFIG_FILE_NAME        (PROJ_NAME ".cfg")
+#define   MAIN_CLASS_WINDOW_NAME  "DUIMainFrame"
+#define   SKIN_FILE               ("mainframe_" PROJ_NAME ".xml")
+#define   SKIN_FOLDER             ("res\\proj_" PROJ_NAME "_res")
+
+
+class  CGlobalData {
+public:
+	ILog    *                 m_log;
+	IConfig *                 m_cfg;
+	LmnToolkits::Thread *     m_thrd_db;
+
+public:
+	CGlobalData() {
+		m_log = 0;
+		m_cfg = 0;
+		m_thrd_db = 0;
+	}
+};
+
+extern CGlobalData  g_data;
+
+
+// templates
+template <class T>
+void ClearVector(std::vector<T> & v) {
+	typedef std::vector<T>::iterator v_it;
+	v_it it;
+	for (it = v.begin(); it != v.end(); it++) {
+		delete *it;
+	}
+	v.clear();
+}
