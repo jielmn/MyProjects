0a6eedda9cb0e8e954c46f95efaeac88b5276215
jielmn@aliyun.com <jielmn@aliyun.com>
2019-06-25 11:07:24 +0800

    术中4.3 - 修改

--- 术中监控_多窗口_4.3/术中监控_多窗口_4.3/common.h
+++ 术中监控_多窗口_4.3/术中监控_多窗口_4.3/common.h
@@ -60,8 +60,8 @@ using namespace DuiLib;
 #endif
 #define   MAX_TAG_PNAME_LENGTH        20
 
-#define   VERSION                     "2.1.1"
-#define   COMPILE_TIME                "2019-06-13 17:03"
+#define   VERSION                     "2.1.2"
+#define   COMPILE_TIME                "2019-06-25 11:06"
 
 // ¿Ø¼þid
 #define   TABS_ID                  "switch"
