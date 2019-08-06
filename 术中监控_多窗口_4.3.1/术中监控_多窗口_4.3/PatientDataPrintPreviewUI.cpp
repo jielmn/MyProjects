8339a2d4c4ebf330a7dda21b6a9e108969a50726
jielmn@aliyun.com <jielmn@aliyun.com>
2019-08-06 19:18:27 +0800

    术中4.3.1 - 修改

---
+++ 术中监控_多窗口_4.3.1/术中监控_多窗口_4.3/PatientDataPrintPreviewUI.cpp
@@ -0,0 +1,38 @@
+#include "PatientDataPrintPreviewUI.h"
+#include "resource.h"
+
+CPatientDataPrintPreviewUI::CPatientDataPrintPreviewUI() {
+	memset( m_szUIPName,     0, MAX_TAG_PNAME_LENGTH );
+	memset( &m_patient_info, 0, sizeof(PatientInfo) );
+	memset( m_patient_data,  0, sizeof(PatientData) * 7 );
+	m_tFirstDate = 0;
+
+	HRSRC hResource = ::FindResource(0, MAKEINTRESOURCE(IDR_XML1), "XML");
+	if (hResource != NULL) {
+		// 加载资源
+		HGLOBAL hg = LoadResource(0, hResource);
+		if (hg != NULL) {
+			// 锁定资源
+			LPVOID pData = LockResource(hg);
+			if (pData != NULL) {
+				// 获取资源大小
+				DWORD dwSize = SizeofResource(0, hResource);
+				m_XmlChartFile.ReadXmlChartStr((const char *)pData, dwSize);
+			}
+		}
+	}
+}
+
+bool CPatientDataPrintPreviewUI::DoPaint(HDC hDC, const RECT& rcPaint, CControlUI* pStopControl) {
+	CControlUI::DoPaint(hDC, rcPaint, pStopControl);
+	DrawXml2ChartUI(hDC, m_XmlChartFile.m_ChartUI, m_rcItem.left, m_rcItem.top);
+	return true;
+}
+
+void CPatientDataPrintPreviewUI::DoEvent(DuiLib::TEventUI& event) {
+	CControlUI::DoEvent(event); 
+}
+
+LPCTSTR CPatientDataPrintPreviewUI::GetClass() const {
+	return "PatientDataPrintPreview";
+}
---
+++ 术中监控_多窗口_4.3.1/术中监控_多窗口_4.3/PatientDataPrintPreviewUI.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "common.h"
+#include "UIlib.h"
+using namespace DuiLib;
+
+#include "Xml2Chart.h"
+
+class CPatientDataPrintPreviewUI : public CControlUI {
+public:
+	CPatientDataPrintPreviewUI();
+
+	virtual bool DoPaint(HDC hDC, const RECT& rcPaint, CControlUI* pStopControl);
+	virtual void DoEvent(DuiLib::TEventUI& event);
+	virtual LPCTSTR GetClass() const;
+
+private:
+	CXml2ChartFile   m_XmlChartFile;
+
+public:
+	char                        m_szUIPName[MAX_TAG_PNAME_LENGTH];         // UI 传过来的name
+	PatientInfo                 m_patient_info;
+	PatientData                 m_patient_data[7];
+	time_t                      m_tFirstDate;
+};
