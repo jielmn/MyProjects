bf36402d934a48385585aee7cb824dab86a7457e
jielmn@aliyun.com <jielmn@aliyun.com>
2019-09-20 12:02:50 +0800

    监护仪校准 - 修改

--- Work/监护仪校准/监护仪校准/CustomControls.cpp
+++ Work/监护仪校准/监护仪校准/CustomControls.cpp
@@ -4,7 +4,15 @@ const int CTempItemUI::WIDTH = 200;
 const int CTempItemUI::HEIGHT = 32;                       
 
 CTempItemUI::CTempItemUI() {
+	m_opBasePoint = 0;
+	m_lblTemp = 0;
+	m_lblDutyCycle = 0;
+	m_btnUp = 0;
+	m_btnDown = 0;
+	m_btnAdjust = 0;
 
+	m_nTemp = 0;
+	m_nDutyCycle = 0;
 }
 
 CTempItemUI::~CTempItemUI() {
@@ -28,4 +36,50 @@ void  CTempItemUI::DoInit() {
 
 	this->SetFixedWidth(WIDTH);
 	this->SetFixedHeight(HEIGHT);
+
+	m_opBasePoint = static_cast<COptionUI*>(m_pManager->FindControl("optBasePoint"));
+	m_lblTemp = static_cast<CLabelUI*>(m_pManager->FindControl("lblTemp"));
+	m_lblDutyCycle = static_cast<CLabelUI*>(m_pManager->FindControl("lblDutyCycle"));
+	m_btnUp = static_cast<CButtonUI*>(m_pManager->FindControl("btnUp"));
+	m_btnDown = static_cast<CButtonUI*>(m_pManager->FindControl("btnDown"));
+	m_btnAdjust = static_cast<CButtonUI*>(m_pManager->FindControl("btnAdjust"));
+
+	SetTemp();
+	SetDutyCycle();
+}
+
+void  CTempItemUI::SetTemp(int nTemp) {
+	m_nTemp = nTemp;
+	SetTemp();
+}
+ 
+int   CTempItemUI::GetTemp() const {
+	return m_nTemp;
+}
+
+void  CTempItemUI::SetDutyCycle(int nDutyCycle) {
+	m_nDutyCycle = nDutyCycle;
+	SetDutyCycle();
+}
+
+int   CTempItemUI::GetDutyCycle() const {
+	return m_nDutyCycle;
+}
+
+void  CTempItemUI::SetTemp() {
+	if ( 0 == m_lblTemp)
+		return;
+
+	CDuiString  strText;
+	strText.Format("%.2f", m_nTemp / 100.0);
+	m_lblTemp->SetText(strText);
+}
+
+void  CTempItemUI::SetDutyCycle() {
+	if ( 0 == m_lblDutyCycle)
+		return;
+
+	CDuiString  strText;
+	strText.Format("%d", m_nDutyCycle);
+	m_lblDutyCycle->SetText(strText);
 }
--- Work/监护仪校准/监护仪校准/CustomControls.h
+++ Work/监护仪校准/监护仪校准/CustomControls.h
@@ -6,12 +6,30 @@ class CTempItemUI : public CContainerUI
 public:
 	CTempItemUI();
 	~CTempItemUI();
+	void  SetTemp(int nTemp);
+	int   GetTemp() const;
+	void  SetDutyCycle(int nDutyCycle);
+	int   GetDutyCycle() const;
 
 private:
 	LPCTSTR GetClass() const;
 	void DoInit();
+	void  SetTemp();
+	void  SetDutyCycle();
 
 public:
 	static const int  WIDTH;
 	static const int  HEIGHT;
+
+private:
+	COptionUI *                    m_opBasePoint;
+	CLabelUI *                     m_lblTemp;
+	CLabelUI *                     m_lblDutyCycle;
+	CButtonUI *                    m_btnUp;
+	CButtonUI *                    m_btnDown;
+	CButtonUI *                    m_btnAdjust;
+
+private:
+	int                            m_nTemp;
+	int                            m_nDutyCycle;
 };
