b0f4c824d3143156707f1a63bc751e50a63d8051
jielmn <jielmn@aliyun.com>
2020-05-20 15:50:37 +0800

    术中监控2.x - 去掉连续测温模式

--- 术中监控2.x/术中监控2.x/ModeButton.cpp
+++ 术中监控2.x/术中监控2.x/ModeButton.cpp
@@ -28,14 +28,14 @@ void CModeButton::SwitchMode() {
 	switch (m_mode)
 	{
 	case CModeButton::Mode_Hand:
-		m_mode = Mode_Single;
-		this->SetBkImage("file='single.png' dest='2,2,28,28'");
-		break;
-	case CModeButton::Mode_Single:
 		m_mode = Mode_Multiple;
 		this->SetBkImage("file='multiple.png' dest='2,2,28,28'");
 		break;
-	case CModeButton::Mode_Multiple:
+	//case CModeButton::Mode_Single:
+	//	m_mode = Mode_Multiple;
+	//	this->SetBkImage("file='multiple.png' dest='2,2,28,28'");
+	//	break;
+	case CModeButton::Mode_Multiple:  
 		m_mode = Mode_Hand;
 		this->SetBkImage("file='hand.png' dest='2,2,28,28'");
 		break;
